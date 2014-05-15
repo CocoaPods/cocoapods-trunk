95f5f74b94ce6f0f6537d497ee9604670156c0a4
Eloy Durán <eloy.de.enige@gmail.com>
2014-05-15 23:39:23 +0200

    Refactor and start constructing actual 'me' output.

--- lib/pod/command/trunk.rb
+++ lib/pod/command/trunk.rb
@@ -12,22 +12,47 @@ module Pod
 
       private
 
-      scheme_and_host = ENV['TRUNK_SCHEME_AND_HOST'] || 'https://trunk.cocoapods.org'
-      BASE_URL = "#{scheme_and_host}/api/v1"
+      SCHEME_AND_HOST = ENV['TRUNK_SCHEME_AND_HOST'] || 'https://trunk.cocoapods.org'
+      BASE_URL = "#{SCHEME_AND_HOST}/api/v1"
 
-      def request(action, *args)
+      def request_url(action, url, *args)
+        #UI.title "Performing #{action.to_s.upcase} request to #{url}" do
+        response = nil
+        #UI.title "Connecting to #{SCHEME_AND_HOST}" do
+          response = create_request(action, url, *args)
+          if (400...600).include?(response.status_code)
+            print_error(response.body)
+          end
+        #end
+        response
+      end
+
+      def request_path(action, path, *args)
+        request_url(action, "#{BASE_URL}/#{path}", *args)
+      end
+
+      def create_request(*args)
         if verbose?
-          REST.send(action, *args) do |request|
+          REST.send(*args) do |request|
             request.set_debug_output($stdout)
           end
         else
-          REST.send(action, *args)
+          REST.send(*args)
         end
       end
 
-      def print_response(response)
-        puts "[HTTP: #{response.status_code}]"
-        puts response.body
+      def print_error(body)
+        begin
+          json = JSON.parse(body)
+        rescue JSON::ParseError
+          json = {}
+        end
+        error = json['error'] || "An unexpected error ocurred: #{body}"
+        raise Informative, error
+      end
+
+      def json(response)
+        JSON.parse(response.body)
       end
 
       def netrc
@@ -36,6 +61,17 @@ module Pod
 
       def token
         netrc['trunk.cocoapods.org'] && netrc['trunk.cocoapods.org'].last
+      end
+
+      def default_headers
+        {
+          'Content-Type' => 'application/json; charset=utf-8',
+          'Accept' => 'application/json; charset=utf-8'
+        }
+      end
+
+      def auth_headers
+        default_headers.merge('Authorization' => "Token #{token}")
       end
 
       class Register < Trunk
@@ -53,16 +89,23 @@ module Pod
 
         def validate!
           super
-          help! 'Specify both your name and email address' unless @name && @email
+          unless @name && @email
+            help! 'Specify both your name and email address'
+          end
         end
 
         def run
-          response = request(:post, "#{BASE_URL}/sessions", { 'email' => @email, 'name' => @name }.to_json, 'Content-Type' => 'application/json; charset=utf-8')
-          print_response(response)
-          token = JSON.parse(response.body)['token']
+          body = { 'email' => @email, 'name' => @name }.to_json
+          json = json(request_path(:post, "sessions", body, default_headers))
+          save_token(json['token'])
+          UI.notice 'Saved session token to ~/.netrc. Please verify the ' \
+                    'session by clicking the link in the verification email ' \
+                    "that has been sent to #{@email}"
+        end
+
+        def save_token(token)
           netrc['trunk.cocoapods.org'] = @email, token
           netrc.save
-          puts 'Saved token to ~/.netrc, please verify session by clicking the link in the verification email that has been sent.'
         end
       end
 
@@ -75,8 +118,30 @@ module Pod
         end
 
         def run
-          response = request(:get, "#{BASE_URL}/sessions", 'Content-Type' => 'application/json; charset=utf-8', 'Authorization' => "Token #{token}")
-          print_response(response)
+          json = json(request_path(:get, "sessions", auth_headers))
+          UI.labeled 'Name', json['name']
+          UI.labeled 'Email', json['email']
+
+          sessions = json['sessions'].map do |session|
+            if session['verified']
+              "#{formatted_time(session['created_at'])} - #{formatted_time(session['valid_until'])}".green
+            else
+              "#{formatted_time(session['created_at'])} (unverified)".yellow
+            end
+          end
+          UI.labeled 'Sessions', sessions
+        end
+
+        def formatted_time(time_string)
+          require 'active_support/time'
+          @tz_offset ||= Time.zone_offset(ENV['TZ'] || `/bin/date +%Z`.strip)
+          @current_year ||= Date.today.year
+          time = Time.parse(time_string) + @tz_offset
+          formatted = time.to_formatted_s(:long_ordinal)
+          if time.year == @current_year
+            formatted.sub!(" #{@current_year}", '')
+          end
+          "#{formatted}"
         end
       end
 
@@ -92,13 +157,17 @@ module Pod
 
         def validate!
           super
-          help! 'You need to register a session first.' unless netrc['trunk.cocoapods.org']
-          help! 'Specify the pod name and the new owner’s email address' unless @pod && @email
+          unless netrc['trunk.cocoapods.org']
+            help! 'You need to register a session first.'
+          end
+          unless @pod && @email
+            help! 'Specify the pod name and the new owner’s email address'
+          end
         end
 
         def run
-          response = request(:patch, "#{BASE_URL}/pods/#{@pod}/owners", { 'email' => @email }.to_json, 'Content-Type' => 'application/json; charset=utf-8', 'Authorization' => "Token #{token}")
-          print_response(response)
+          body = { 'email' => @email }.to_json
+          request_path(:patch, "pods/#{@pod}/owners", body, auth_headers)
         end
       end
 
@@ -113,24 +182,26 @@ module Pod
 
         def validate!
           super
-          help! 'You need to register a session first.' unless netrc['trunk.cocoapods.org']
-          help! 'Specify the path to the pod spec' unless @path
+          unless netrc['trunk.cocoapods.org']
+            help! 'You need to register a session first.'
+          end
+          unless @path
+            help! 'Specify the path to the podspec file.'
+          end
         end
 
         def run
           spec = Pod::Specification.from_file(@path)
-          response = request(:post, "#{BASE_URL}/pods", spec.to_json, 'Content-Type' => 'application/json; charset=utf-8', 'Authorization' => "Token #{token}")
+          response = request_path(:post, "pods", spec.to_json, auth_headers)
 
           if (400...600).include?(response.status_code)
-            print_response(response)
             return
           end
 
           status_url = response.headers['location'].first
           puts "Registered resource URL: #{status_url}"
 
-          response = request(:get, status_url, 'Content-Type' => 'application/json; charset=utf-8', 'Accept' => 'application/json; charset=utf-8')
-          print_response(response)
+          request_url(:get, status_url, default_headers)
         end
       end
     end
