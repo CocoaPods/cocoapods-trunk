209c601b342104da98cb61b01fa26dfa13b00d21
Kyle Fuller <inbox@kylefuller.co.uk>
2014-11-22 15:33:08 +0100

    [Spec] Test register requires email #6

--- spec/command/trunk/register_spec.rb
+++ spec/command/trunk/register_spec.rb
@@ -4,8 +4,15 @@ module Pod
   describe Command::Trunk::Register do
     describe 'CLAide' do
       it 'registers it self' do
-        Command.parse(%w(        trunk register        )).should.be.instance_of Command::Trunk::Register
+        Command.parse(%w( trunk register )).should.be.instance_of Command::Trunk::Register
       end
+    end
+
+    it "should error if email is not supplied" do
+      Netrc.any_instance.stubs(:[]).returns(nil)
+      command = Command.parse(%w( trunk register ))
+      exception = lambda { command.validate! }.should.raise CLAide::Help
+      exception.message.should.include 'email address'
     end
   end
 end
